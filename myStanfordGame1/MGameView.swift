ed1ea0a1d082ce921179bbeb3a2dec4ae67b2f99
hadohado <hadohado@gmail.com>
2021-05-17 16:07:49 -0700

    Initial Commit

---
+++ myStanfordGame1/MGameView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  myStanfordGame1
+//
+//  Created by ha tuong do on 5/17/21.
+//
+
+import SwiftUI
+
+struct MGameView: View {
+    var body: some View {
+        ForEach(0..<3) {_ in
+            GeometryReader { geometry  in
+                ZStack {
+                    RoundedRectangle(cornerRadius: 10 ).fill(Color.white)
+                    RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                    Text("💋")
+                }  
+                .padding()
+                .font(Font.system(size: min(geometry.size.width, geometry.size.height) * fontScaleFactor ))
+                
+            }
+            
+        }
+        
+        //        Text("Hi, world!")
+        
+    }
+    
+    let cornerRadius: CGFloat = 10.0
+    let edgeLineWidth: CGFloat = 3.0
+    let fontScaleFactor: CGFloat = 0.75
+    
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MGameView()
+    }
+}
